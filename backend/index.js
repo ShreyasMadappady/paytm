a99444f1336d6e10d6c1d14d5de9a8f20fb37f49
Shreyas M S <shreyasmadappady20@gmail.com>
2024-01-28 22:42:32 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,17 @@
-const express = require("express");
+import express from "express";
+import mongoose from "mongoose";
 
+const app = express();
 
+mongoose
+  .connect(
+    "mongodb+srv://dummyuser:dummyuser@cluster0.4j2dxvz.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(3000, () => {
+      console.log(`Server Running on PORT ${3000}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`Error Connecting to the server: ${error}`);
+  });
---
+++ backend/model/db.js
@@ -0,0 +1,10 @@
+import mongoose, { modelNames } from "mongoose";
+
+const userSchema = mongoose.Schema({
+  username: String,
+  password: String,
+  firstName: String,
+  lastName: String,
+});
+
+export const User = mongoose.model("User", userSchema);
